b40b29dd5d9162430d0356e755f7c09bb9d47159
James Gosnell <jamesgosnell@gmail.com>
2014-08-27 18:21:12 -0500

    ADDED new simple test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,37 @@
-var json2csv = require('../index.js');
+var util = require('util');
+var Json2csv = require('../index.js');
+var stream = require('stream');
+util.inherits(ReadStream, stream.Readable);
 
-var json = {
-	name: James,
-	age: 24,
-	city: Chicago
+function ReadStream(opt){
+	var options = opt || {};
+	options.objectMode=true;
+	stream.Readable.call(this, options);
 }
+var jsonArray = [
+	{
+		name: 'James',
+		age: 34,
+		city: 'Chicago'
+	},
+	{
+		name: 'Danielle',
+		age: 29,
+		city: 'Chicago'
+	}
+];
+
+ReadStream.prototype._read = function() {
+	var stream = this;
+	jsonArray.forEach(function(currVal, index, array){
+		stream.push(currVal);
+	});
+	this.push(null);
+};
+
+var readStream = new ReadStream();
+var json2csv = new Json2csv();
+
+json2csv.on('data', function(data) { console.dir(data);});
+
+readStream.pipe(json2csv);
